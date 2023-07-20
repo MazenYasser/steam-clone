88d1861a2270a7d85219eb618387236c46c9cb6c
Mazen Yasser <mazenyasser225@gmail.com>
2023-07-20 18:24:22 +0300

    Now home page carousels show games from database

--- steam-django/steam_clone_django/pages/views.py
+++ steam-django/steam_clone_django/pages/views.py
@@ -3,7 +3,7 @@ from games.models import Games
 # Create your views here.
 
 def home(request, *args, **kwargs):
-    return render(request, 'pages/home.html', context= {'games': Games.objects.all()})
+    return render(request, 'pages/home.html', context= {'games': Games.objects.all() , 'n': range(1,4)})
 
 def about(request, *args, **kwargs):
     return render(request, 'pages/about.html')
