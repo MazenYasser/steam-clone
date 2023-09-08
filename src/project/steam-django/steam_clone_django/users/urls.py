4c49c4eac84a83cc211516e67f522b7d6d59dfc6
IvanIsak2000 <vingving648@gmail.com>
2023-09-08 23:48:59 +0300

    change root src to `src`, separate to `src/project` and `src/docs`

---
+++ src/project/steam-django/steam_clone_django/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', view=views.login, name='login'),
+    path('register', view=views.register, name='register'),
+    path('logout', view=views.logout, name='logout'),
+    path('activate/<uidb64>/<token>', view=views.activate, name='activate'),
+    path('accountInfo', view=views.accountInfo, name='accountInfo'),
+    path('confirmMailChange/<uidb64>/<token>/<newEmail>', view=views.confirmMailChange, name='confirmMailChange'),
+    path('forgotPassword', view=views.forgotPassword, name='forgotPassword'),
+    path('passwordReset/<uidb64>/<token>', view=views.passwordReset, name='passwordReset'),
+]
